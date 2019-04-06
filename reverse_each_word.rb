ec8c2f773d9fc828f1f785f94df34c958cb4f438
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-06 01:23:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word(sentence)
   word_array = sentence.split
   reverse_word_array = []
   word_array.collect do |word|
-    reverse_word_array = word.reverse 
-    #reverse_word_array << reverse_word 
+    reverse_word_array << word.reverse
   end 
   reverse_word_array.join(" ")
 
